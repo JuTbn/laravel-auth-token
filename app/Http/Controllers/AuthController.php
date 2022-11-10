072bcc80fae8c0093a1b0bc53861108156115607
JuTbn <jtoubon96@gmail.com>
2022-11-10 17:20:23 +0100

    add authenticate function

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,5 +9,22 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    //
+    public function authenticate(Request $request): JsonResponse
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if(Hash::check($request->password, $user->password))
+        {
+            return response()->json([
+                'token' => $user->createToken(time())->plainTextToken
+            ]);
+        }
+    }
+
+    public function dashboard(): JsonResponse
+    {
+        return response()->json([
+            'success'=>'Bienvenue!'
+        ]);
+    }
 }
